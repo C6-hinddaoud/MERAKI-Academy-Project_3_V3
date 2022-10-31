7972c8fd411121bc1b964de656025c81ec845603
hinddaoud <hhinddaoud@gmail.com>
2022-10-31 13:01:49 +0200

    get all comments

--- MERAKI_Academy_Project_3_Part_5/controllers/comments.js
+++ MERAKI_Academy_Project_3_Part_5/controllers/comments.js
@@ -23,26 +23,54 @@ message: "Server Error",
 err: err.message
   })
 })
-   /**Res:
 
+};
+// This function returns the comments
+const getAllComments = (req, res) => {
+const query=`select comments.id,comments.article_id,comments.commenter_id,comments.is_deleted,comments.comment  ,users.firstname,USERS.id from COMMENTS INNER JOIN users ON
+comments.commenter_id=users.id
+ WHERE comments.is_deleted=0`
+pool.query(query)
+.then((result)=>{
+if(result.rows.length==0){
+ return res.status(401).json({
+    success: true,
+message: "ther is no comments",
+
+  })
+}
+
+  res.status(200).json({
+    success: true,
+message: "All comments",
+comments:result.rows
+  })
+})
+.catch((err)=>{
+res.status(500).json({
+  success: false,
+message: "Server Error",
+err: err.message
+})
+
+})
+
+
+/**Res:
 If no error occurred
-
-Status code: 201
-Return an object with the following keys:
+Status code: 200
+Return an object with 3 keys:
 success: true
-message: "The comment has been created successfully"
-results: the result
+message: "All comments"
+comments: the found comments
 If an error occurred
-
 Status code: 500
 Return an object with 3 keys:
 success: false
 message: "Server Error"
-err: error message */
-};
-// This function returns the comments
-const getAllComments = (req, res) => {
-  //TODO: write your code here
+err: error message
+ */
+
 };
 module.exports = {
   createNewComment,
